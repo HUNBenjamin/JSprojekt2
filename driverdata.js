4f42826a758e1d645496c6df73bc9061dc0f6022
HUNBenjamin <reinhardt.benjamin@students.jedlik.eu>
2024-04-09 13:53:18 +0200

    #1 driver list done for now

---
+++ driverdata.js
@@ -0,0 +1,23 @@
+//rajtszám;keresztnév;vezetéknév;születési év;csapat;szezon győzelem odds;
+const data = [
+    "1;Max;Verstappen;1997;Red Bull;1.07",
+    "2;Logan;Sargeant;2000;Williams;5001",
+    "3;Daniel;Ricciardo;1989;Visa RB;4001",
+    "4;Lando;Norris;1999;McLaren;51",
+    "10;Pierre;Gasly;1996;Alpine;5001",
+    "11;Sergio;Perez;1990;Red Bull;26",
+    "14;Fernando;Alonso;1981;Aston Martin;101",
+    "16;Charles;Leclerc;1997;Ferrari;26",
+    "18;Lance;Stroll;1998;Aston Martin;1001",
+    "20;Kevin;Magnussen;1992;Haas;4001",
+    "22;Yuki;Tsunoda;2000;Visa RB;3001",
+    "23;Alexander;Albon;1996;Williams;3001",
+    "24;Zhou;Guanyu;1999;Sauber;5001",
+    "27;Nico;Hülkenberg;1987;Haas;4001",
+    "31;Esteban;Ocon;1996;Alpine;5001",
+    "44;Buzzy;Nyaffilton;1985;Mercedes;101",
+    "55;Carlos;Sainz;1994;Ferrari;26",
+    "63;George;Russell;1998;Mercedes;67",
+    "77;Valtteri;Bottas;1989;Sauber;4001",
+    "81;Oscar;Piastri;2001;McLaren;81"
+]
